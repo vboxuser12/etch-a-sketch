018aed694acbcc420ab719f1f172fea4f6a0ff95
vboxuser12 <192755770+vboxuser12@users.noreply.github.com>
2025-02-16 07:38:34 -0500

    fix remove divisions in container

--- java.js
+++ java.js
@@ -3,48 +3,27 @@ let body= document.querySelector("body")
 body.appendChild(button);
 let maincontainer=document.querySelector("#maincontainer");
 
-let div1=document.createElement("div");
-let div2=document.createElement("div")
-let div3=document.createElement("div")
-let div4=document.createElement("div")
-let div5=document.createElement("div")
-let div6=document.createElement("div")
-let div7=document.createElement("div")
-let div8=document.createElement("div")
-let div9=document.createElement("div")
-let div10=document.createElement("div")
-let div11=document.createElement("div")
-let div12=document.createElement("div")
-let div13=document.createElement("div")
-let div14=document.createElement("div")
-let div15=document.createElement("div")
-let div16=document.createElement("div")
+for(i=1;i<=16;i++)
+{let div=document.createElement("div")
+div.setAttribute("id","regular");
+maincontainer.appendChild(div);
+}
+
+function remove(){
+while(maincontainer.firstChild)
+{maincontainer.firstChild.remove()}
+}
 
 
 
-maincontainer.append(div1,div2,div3,div4,div5,div6,div7,div8,div9,div10,div11,div12,div13,div14,div15,div16);
 
 button.textContent="button"
 let pct="%"
 function howmanygrid() 
 { let a =(prompt("hpw many grid do you want"))
-    div1.remove()
-    div2.remove()
-    div3.remove()
-    div4.remove()
-    div5.remove()
-    div6.remove()
-    div7.remove()
-    div8.remove()
-    div9.remove()
-    div10.remove()
-    div11.remove()
-    div12.remove()
-    div13.remove()
-    div14.remove()
-    div15.remove()
-    div16.remove();
-     
+    
+    
+    remove()
    if(a>100)
    {alert("pick smaller number")}
    else{
@@ -53,7 +32,6 @@ function howmanygrid()
       { let div =document.createElement("div")
         div.setAttribute("id","choice")
         maincontainer.append(div);
-        div.textContent="i"
         div.style.backgroundColor="blue";
         div.style.boxSizing="border-box" 
         div.style.width=100/a+pct
@@ -64,8 +42,6 @@ function howmanygrid()
 
 
 button.addEventListener("click", howmanygrid)
-
-console.log(100/10+pct)
 
     
   
